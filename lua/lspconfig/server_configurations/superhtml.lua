491c84457efddc85b7215ef36b60f469ac83ed9e
EliSauder <24995216+EliSauder@users.noreply.github.com>
2024-07-23 13:22:05 -0700

    fix: update languagid correctly as lsp expects

--- lua/lspconfig/server_configurations/superhtml.lua
+++ lua/lspconfig/server_configurations/superhtml.lua
@@ -1,11 +1,19 @@
 local util = require 'lspconfig.util'
 
+local language_id_of = {
+  shtml = 'suprehtml'
+}
+
 return {
   default_config = {
     cmd = { 'superhtml', 'lsp' },
     filetypes = { 'shtml' },
     root_dir = util.root_pattern '.git',
     single_file_support = true,
+    get_language_id = function(_, ftype)
+      return language_id_of[ftype]
+    end,
+
   },
   docs = {
     description = [[
