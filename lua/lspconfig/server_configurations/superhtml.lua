a2bcbbc906d431b15c8f091d95c8f977b09e3942
EliSauder <24995216+EliSauder@users.noreply.github.com>
2024-07-26 08:01:02 -0700

    test: check without html support

--- lua/lspconfig/server_configurations/superhtml.lua
+++ lua/lspconfig/server_configurations/superhtml.lua
@@ -3,7 +3,7 @@ local util = require 'lspconfig.util'
 return {
   default_config = {
     cmd = { 'superhtml', 'lsp' },
-    filetypes = { 'superhtml', 'html' },
+    filetypes = { 'superhtml' },
     root_dir = util.root_pattern '.git',
     single_file_support = true,
   },
